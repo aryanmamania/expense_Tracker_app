43354ae8e1c83e996e0335f007658661d0cadc81
aryanmamania <aryanmamania17@gmail.com>
2024-07-04 11:31:41 +0530

    added transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,32 @@
+import Transaction from "../models/transaction.model.js"
+
 const transactionResolver = {
-    Query: {},
+    Query: {
+        transactions: async(_,_, context) =>{
+            try{
+              if(!context.getUser()) 
+                throw new Error("Unauthorized")
+                const userId = context.getUser()._id;
+                const transactions = await Transaction.find({ userId});
+              return transactions
+            }catch(err){
+                console.error("Error in getting transaction", err);
+                throw new Error("Error getting transactions");  
+            }
+        },
+        transaction: async(_, { transactionId}, ) =>{
+try{
+const transaction = await transaction.findById(transactionId)
+return transaction;
+}catch(err){
+console.error("Error getting trasnaction", err);
+throw new Error("Error getting transaction");
+
+}
+        },
+        
+    },
     Mutation: {}
 }
 
-export default transactionResolver
+export default transactionResolver;
